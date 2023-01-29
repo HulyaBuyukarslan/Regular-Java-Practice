8bd0c1a774c687fbb1d34661d91dfa33c54db05d
Hülya Büyükarslan <monaroza825@gmail.com>
2023-01-30 00:11:22 +0300

    30.01.2023

---
+++ src/main/java/day08/C01_ForLoop.java
@@ -0,0 +1,22 @@
+package day08;
+
+public class C01_ForLoop {
+
+        public static void main(String[] args) {
+
+            //Kullanicidan baslangic , bitis ve artis miktarini alarak
+            // aradaki tum sayilari aralarina virgul koyarak yazdirin
+
+            // Scanner input = new Scanner(System.in);
+            int baslangic=10;
+            int bitis=50;
+            int artisMiktari=5;
+            for (int i = baslangic; i <=bitis ; i+=artisMiktari) {
+                if(i<bitis){
+                    System.out.print(i+",");
+                }else
+                    System.out.println(i);
+            }
+        }
+    }
+
